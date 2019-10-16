0fa74d906a6ea9ad99258c6b8066cdb5f476575f
Iftifar Taz <iftifar.taz@northsouth.edu>
2019-10-16 10:57:33 +0600

    package update

--- src/inbox-contract.js
+++ src/inbox-contract.js
@@ -1,34 +1,48 @@
 import web3 from "./web3";
 
-const address = '0x5eec2A1d8669ADcC43395795d9825a7b65483961';
+const address = '0x259076444b0dfCD892057E0F1e4c1611E045071C';
 const abi = [
     {
-        constant: false,
-        inputs: [{ name: 'newMessage', type: 'string' }],
-        name: 'setMessage',
-        outputs: [],
+        inputs: [
+            {
+                internalType: "string",
+                name: "initialMessage",
+                type: "string"
+            }
+        ],
         payable: false,
-        stateMutability: 'nonpayable',
-        type: 'function',
-        signature: '0x368b8772'
+        stateMutability: "nonpayable",
+        type: "constructor"
     },
     {
         constant: true,
         inputs: [],
-        name: 'message',
-        outputs: [{ name: '', type: 'string' }],
+        name: "message",
+        outputs: [
+            {
+                internalType: "string",
+                name: "",
+                type: "string"
+            }
+        ],
         payable: false,
-        stateMutability: 'view',
-        type: 'function',
-        signature: '0xe21f37ce'
+        stateMutability: "view",
+        type: "function"
     },
     {
-        inputs: [{ name: 'initialMessage', type: 'string' }],
+        constant: false,
+        inputs: [
+            {
+                internalType: "string",
+                name: "newMessage",
+                type: "string"
+            }
+        ],
+        name: "setMessage",
+        outputs: [],
         payable: false,
-        stateMutability: 'nonpayable',
-        type: 'constructor',
-        constant: undefined,
-        signature: 'constructor'
+        stateMutability: "nonpayable",
+        type: "function"
     }
 ];
 
